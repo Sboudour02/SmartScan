cd8eb57b2602eb3c5149f926c245c0763c1ed875
Sid Ahmed BOUDOUR <sboudour02@gmail.com>
2026-04-21 23:44:22 +0100

    revert: remove in-app updater and reset to stable version 1.0.1+1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,6 @@ import 'barcode_generator_screen.dart';
 import 'scanner_screen.dart';
 import 'history_screen.dart';
 import 'batch_generator_screen.dart';
-import '../utils/update_manager.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,7 +26,6 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _runSecurityChecks();
-      UpdateManager.checkAndPromptUpdate(context);
     });
   }
 
